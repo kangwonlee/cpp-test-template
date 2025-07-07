c2a84950c46acf5ef1d19f8e7065f44197787e80
KangWon LEE <kangwon@gmail.com>
2025-07-07 16:42:53 +0900

    feature : implement input output assignment in C++

--- tests/test_build.py
+++ tests/test_build.py
@@ -1,28 +1,26 @@
 # begin tests/test_build.py
 import pathlib
-import re
 import subprocess
 
 import pytest
 
 
-def test_could_find_src_files(src_file_path:pathlib.Path):
+def test_could_find_src_files(src_file_path: pathlib.Path):
     """
     Test the find_src_files function.
     """
-
     assert src_file_path.exists(), f"Source file {src_file_path} does not exist"
     assert src_file_path.is_file(), f"{src_file_path} is not a file"
-    assert (src_file_path.suffix == '.c') or (src_file_path.suffix == '.cpp'), f"Expected a .c or .cpp file, got {src_file_path.suffix}"
+    assert src_file_path.suffix == '.cpp', f"Expected a .cpp file, got {src_file_path.suffix}"
 
 
-def test_build(src_file_path:pathlib.Path, obj_file_path:pathlib.Path):
+def test_build(src_file_path: pathlib.Path, obj_file_path: pathlib.Path):
     """
     Test the build command.
     """
     result = subprocess.run(
         [
-            "gcc",
+            "g++",
             "-c", str(src_file_path),
             '-o', str(obj_file_path),
         ],
